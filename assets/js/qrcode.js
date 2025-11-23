df36d21eff974273d93e4915f42e35441132431e
picker770 <arehman897@gmail.com>
2025-11-23 03:15:23 +0000

    add functionality to our QR code Generator

---
+++ assets/js/qrcode.js
@@ -0,0 +1,16 @@
+const qrInput = document.getElementById("qr-input");
+const qrImg = document.getElementById("qr-img");
+const qrBtn = document.getElementById("qr-button");
+
+qrBtn.addEventListener('click', () => {
+      const inputValue = qrInput.value;
+      console.log(inputValue);
+
+      if (!inputValue) {
+        alert("Please enter a valid URL");
+        return;
+      } else {
+         qrImg.src= `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+         qrImg.alt=`QR Code for ${inputValue}`;
+      }
+});
